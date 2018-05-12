2fd8c409d6af76b01e5aa84899a5e70235b31bae
Feuway <feuway@gmail.com>
2018-05-12 19:44:14 +0300

    fixs

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const path = require("path");
 const LRU = require("lru-cache");
 const express = require("express");
 const compression = require("compression");
-const microcache = require("route-cache");
+// const microcache = require("route-cache");
 const resolve = file => path.resolve(__dirname, file);
 const { createBundleRenderer } = require("vue-server-renderer");
 const multer = require('multer');
